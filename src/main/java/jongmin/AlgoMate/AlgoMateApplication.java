25206b6b9d7e8e4718b5328945ccc414d3c5713f
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-12 01:04:36 +0900

    Initial commit

---
+++ src/main/java/jongmin/AlgoMate/AlgoMateApplication.java
@@ -0,0 +1,13 @@
+package jongmin.AlgoMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jongmin/AlgoMate/AlgoMateApplicationTests.java
@@ -0,0 +1,13 @@
+package jongmin.AlgoMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
